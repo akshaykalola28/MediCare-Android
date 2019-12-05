bb3f608c946f560a32d7465adcda43ce93e03014
Akshay Kalola <akshay.kalola28@gmail.com>
2019-12-05 20:48:49 +0530

    font change and signup design

--- app/src/main/java/com/finalyearproject/medicare/activities/AuthActivity.kt
+++ app/src/main/java/com/finalyearproject/medicare/activities/AuthActivity.kt
@@ -8,17 +8,29 @@ import com.finalyearproject.medicare.fragments.ChooseFragment
 
 class AuthActivity : AppCompatActivity() {
 
+    private val fragmentManager = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
-        var fragmentTransaction = supportFragmentManager.beginTransaction()
+        val fragmentTransaction = fragmentManager.beginTransaction()
         var chooseFragment = ChooseFragment()
-        fragmentTransaction.add(R.id.auth_fragment, chooseFragment)
+        fragmentTransaction.add(R.id.auth_fragment, chooseFragment, chooseFragment.tag)
         fragmentTransaction.commit()
     }
 
     override fun onBackPressed() {
-        super.onBackPressed()
+        if (fragmentManager.backStackEntryCount == 0) {
+            Log.d(TAG, "backStack is null")
+            super.onBackPressed()
+        } else {
+            Log.d(TAG, "calling pop")
+            fragmentManager.popBackStack()
+        }
+    }
+
+    companion object {
+        const val TAG: String = "AuthActivity"
     }
 }
--- app/src/main/java/com/finalyearproject/medicare/fragments/SignUpFragment.kt
+++ app/src/main/java/com/finalyearproject/medicare/fragments/SignUpFragment.kt
@@ -8,19 +8,19 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.finalyearproject.medicare.R
 import com.finalyearproject.medicare.activities.AuthActivity
-import kotlinx.android.synthetic.main.fragment_log_in.*
+import kotlinx.android.synthetic.main.fragment_sign_up.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class LogInFragment : Fragment() {
+class SignUpFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false)
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -28,10 +28,6 @@ class LogInFragment : Fragment() {
 
         backImageButton.setOnClickListener {
             (requireActivity() as AuthActivity).onBackPressed()
-        }
-
-        loginButton.setOnClickListener {
-            usernameErrorField.error = "Error"
         }
     }
 }
